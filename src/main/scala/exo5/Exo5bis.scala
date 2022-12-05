b951f9f8aac449a859a8d786653336a1e3d3e702
Pierre Galland <pgalland@users.noreply.github.com>
2022-12-05 13:52:49 +0100

    Day 5 part 2

---
+++ src/main/scala/exo5/Exo5bis.scala
@@ -0,0 +1,55 @@
+package exo5
+
+import scala.collection.mutable
+
+object Exo5bis {
+  def main(args: Array[String]): Unit = {
+    val src = scala.io.Source.fromFile("src/main/scala/exo5/input.txt")
+    try {
+      val lines            = src.getLines().toSeq
+      val nbLinesForCrates = lines.indexWhere(_.isEmpty)
+      val stacks           = parseCrates(lines.take(nbLinesForCrates))
+      val moves = parseMoves(lines.drop(nbLinesForCrates + 1))
+        .map { case (quantity, from, to) => (quantity, from - 1, to - 1) } // index from zero.
+
+      moves.foreach { case (quantity, from, to) =>
+        val (toMove, toStay) = stacks(from).splitAt(quantity)
+        stacks(from) = toStay
+        stacks(to).pushAll(toMove.reverse)
+      }
+      println(stacks.map(_.pop()).mkString)
+    } finally {
+      src.close()
+    }
+  }
+
+  def parseCrates(lines: Seq[String]): Array[mutable.Stack[String]] = {
+    val nbStacks = lines.last.split(" +").last.toInt
+    val stacks   = (1 to nbStacks).map(_ => mutable.Stack.empty[String])
+    lines
+      .dropRight(1)
+      .reverse // reverse is important here, we want to push the crates starting from the bottom.
+      .foreach { line =>
+        line.toCharArray
+          .map(_.toString)
+          .grouped(4)
+          .map(forOneStack => if (forOneStack(1) != " ") Some(forOneStack(1)) else None)
+          .zipWithIndex
+          .foreach { case (crate, stackIdx) =>
+            crate.foreach(crateId => stacks(stackIdx).push(crateId))
+          }
+      }
+    stacks.toArray
+  }
+
+  def parseMoves(lines: Seq[String]): Seq[(Int, Int, Int)] = {
+    lines.map { line =>
+      val tokens = line
+        .replace("move ", "")
+        .replace("from ", "")
+        .replace("to ", "")
+        .split(" ")
+      (tokens(0).toInt, tokens(1).toInt, tokens(2).toInt)
+    }
+  }
+}
